563dadeff517855e5e5b5a434f0da25eb2920f21
talentdev2020 <polaris.center@outlook.com>
2022-05-12 11:17:28 -0700

    deploy to testnet

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,12 +5,13 @@ def run(nre):
     # name = str(str_to_felt("MyToken"))
     # symbol = str(str_to_felt("MTK"))
     # decimals = "18"
-    owner = "0x003d251b15b3b9c61abc0cd534c5d319987ae5b5a6d2b35fe192a5e45e906ad6"
+    owner = "0x0511c34a48146c49a341d1405b01bd4f5ad09af0ae9db45a6e40711cc529fc7a"
     params = [owner]
     address, abi = nre.deploy("MyToken")
     print(f"ABI: {abi},\nContract address: {address}")
     # 0x00a23a9007bc986de63566fa328cf163ee8a239a91ff944201b9ad3dc1dc5c85 //owner
     # 0x00185f8c2f9d99d93d88ccc1b73deb572207e7f0bb5a76570512520b4266c5f3
+    # 0x0055e1a6d333950afe8efec26011df3d5552fc535f235b76f254710c34116a94
 # Auxiliary functions
 # def str_to_felt(text):
 #     b_text = bytes(text, "ascii")
